aff4b95a273feb86148cdd1b5f92a1110c3ba8bc
Onyeje Josiah <jocrossonyeje@gmail.com>
2026-07-14 15:16:14 +0100

    Remove unnecessary blank line in TextPlainInputFormatter

--- CoralpayInterbankPayments/Service/TextPlainInputFormatter.cs
+++ CoralpayInterbankPayments/Service/TextPlainInputFormatter.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc.Formatters;
 using System.Text;
 using Microsoft.Net.Http.Headers;
-
 namespace CoralpayInterbankPayments.Service
 {
     public class TextPlainInputFormatter : TextInputFormatter
